d1fd51a5e1830d5404671d50fb6fbf6b4353d379
skdod2453 <skdod2453@gmail.com>
2024-11-30 14:03:02 +0900

    feat: RF-278 <어드민회원가입페이지>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './component/Home';
 import User_Login from './component/login/User_Login';
 import Business_Login from './component/login/Business_Login';
 import Admin_Login from './component/login/Admin_Login';
+import Admin_Register from './component/login/Admin_Register';
 import Business_Request from './component/login/Business_Request';
 import Business_Product from './component/business/Business_Product';
 import Business_ProductAdd from './component/business/Business_ProductAdd';
@@ -38,6 +39,7 @@ function App() {
           <Route path='/user/login' element={<User_Login />} />
           <Route path='/business/login' element={<Business_Login />} />
           <Route path='/djemals/login' element={<Admin_Login />} />
+          <Route path='/djemals/register' element={<Admin_Register />} />
           <Route path='/business/request' element={<Business_Request />} />
           <Route path='/business/product' element={<Business_Product />} />
           <Route path='/business/productadd' element={<Business_ProductAdd />} />
